4506842ed794f04d06d2b3e8be5d2e267406a3c9
Wali Morris <48896622+walimorris@users.noreply.github.com>
2021-02-06 19:16:42 -0800

    Update Constants.java

--- SMStwi/src/main/java/com/morris/Constants.java
+++ SMStwi/src/main/java/com/morris/Constants.java
@@ -1,10 +1,10 @@
 package com.morris;
 
 public class Constants {
-    public static final String ACCOUNT_SID = "AC6c1e0e1dc0386393d94f9feb24722c98";
-    public static final String AUTH_TOKEN = "b15b64851f6081eb88a6b6d4f2fe4197";
-    public static final String TO = "+12062616352";
-    public static final String FROM = "+17479980713";
+    public static final String ACCOUNT_SID = ""; // add Account SID
+    public static final String AUTH_TOKEN = ""; // add Auth Token 
+    public static final String TO = ""; // Add to Number
+    public static final String FROM = ""; // Add from Number
     public static final String USD_TO_BGN_GOOGLE_FINANCE = "https://www.google.com/finance/quote/USD-BGN";
     public static final String USD_TO_BGN_GOOGLE_MARKUP = "class=\"MAwwx\">United States Dollar to Bulgarian lev";
 
